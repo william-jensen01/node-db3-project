a2dcdbbb66f1ab8c996896360d776b8cf08118ef
William Jensen <williambjensen01@gmail.com>
2021-02-02 19:41:03 -0700

    SQL queries done

--- queries.sql
+++ queries.sql
@@ -1,6 +1,7 @@
 -- Multi-Table Query Practice
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
+SELECT p.productname, c.categoryname FROM product as p JOIN Category as c ON p.categoryid = c.id
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
 
