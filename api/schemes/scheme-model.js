798ba08e8aa8506d28ffd8db3b01a0fcf2496872
William Jensen <williambjensen01@gmail.com>
2021-02-02 21:07:30 -0700

    MVP. Changed scheme-router to be try catch

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -1,1 +1,45 @@
 // scheme-model
+const db = require('../../data/db-config');
+
+function find() {
+    return db('schemes');
+};
+
+function findById(id) {
+    return db('schemes').where({ id }).first();
+};
+
+function findSteps(id) {
+    return db('schemes as s')
+    .select('s.scheme_name', 'st.step_number', 'st.instructions')
+    .join('steps as st', 's.id', '=', 'st.scheme_id')
+    .where('st.scheme_id', '=', id)
+    .orderBy('st.step_number');
+};
+
+async function add(scheme) {
+    const [newSchemeId] = await db('schemes').insert(scheme);
+    const newScheme = await findById(newSchemeId);
+    return newScheme;
+};
+
+async function update(id, changes) {
+    const count = await db('schemes').where({ id }).update(changes);
+    const updatedScheme = await findById(id);
+    return updatedScheme;
+};
+
+async function remove(id) {
+    const schemeToBeRemoved = await findById(id);
+    const count = await db('schemes').where({ id }).del()
+    return schemeToBeRemoved;
+};
+
+module.exports = {
+    find,
+    findById,
+    findSteps,
+    add,
+    update,
+    remove
+};
